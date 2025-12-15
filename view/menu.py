07be270c0bbde8cffb148fc4cc1c797ff0663ed0
Andrr008 <andrakurniawanfaisal@gmail.com>
2025-12-15 14:35:17 +0700

    fixing bug

--- view/menu.py
+++ view/menu.py
@@ -1,4 +1,4 @@
-
+import os
 import customtkinter as ctk
 import tkinter as tk
 from PIL import Image
@@ -8,33 +8,44 @@ def menu():
 # Configurasi Window
     from input import opencv
     from detect import detect_face
+
+    # ===== PATH FIX =====
+    VIEW_DIR = os.path.dirname(os.path.abspath(__file__))
+    BASE_DIR = os.path.dirname(VIEW_DIR)
+
+    ICON_PATH = os.path.join(BASE_DIR, "icon.ico")
+    IMAGE_PATH = os.path.join(BASE_DIR, "face-recognition.png")
+
     app = ctk.CTk()
-    # app.overrideredirect(True)  # Hilangkan title bar (tidak bisa dipindah)
     ctk.set_appearance_mode("system")
     ctk.set_default_color_theme("blue")
+
     app.title("Face Detection")
-    app.iconbitmap("icon.ico") 
+    app.iconbitmap(ICON_PATH)
+
     screen_width = app.winfo_screenwidth()
     screen_height = app.winfo_screenheight()
     app.geometry(f"{screen_width}x{screen_height}+-10+0")
-    # app.bind("<Escape>", lambda e: app.destroy())
-    
-    # membuat frame dan menaruh gambar dan tulisan didalam
-    frame = ctk.CTkFrame(app, width=screen_width, height=screen_height*0.4, fg_color=app._fg_color)
-    frame.pack(pady = 80)
-    # Menampilkan Gambar ke dalam frame
-    image = ctk.CTkImage(Image.open("face-recognition.png"), size=(150, 150))
+
+    frame = ctk.CTkFrame(
+        app,
+        width=screen_width,
+        height=screen_height * 0.4,
+        fg_color=app._fg_color
+    )
+    frame.pack(pady=80)
+
+    image = ctk.CTkImage(Image.open(IMAGE_PATH), size=(150, 150))
     label = ctk.CTkLabel(frame, image=image, text="")
     label.pack(side="left", padx=30, pady=20)
-    # membuat animasi tulisan mengetik dan menghapus otomatis
+
     texts = ["Face Recognition", "With DeepFace", "Use ArcFace Model"]
     label_font = ctk.CTkLabel(frame, text="", font=("Tahoma", 90, "bold"))
     label_font.pack(side="left", padx=10, pady=20)
-    
-    # Daftar teks yang akan dianimasikan
-    index = 0     # karakter
-    text_index = 0  # teks ke berapa di list
-    typing = True  # mode: mengetik atau menghapus
+
+    index = 0
+    text_index = 0
+    typing = True
     
     def animate():
         nonlocal index, typing, text_index
